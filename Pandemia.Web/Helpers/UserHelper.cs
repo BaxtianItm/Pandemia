fc3e5c8406f6d4b98acb5c518e4d1bd4ab84d3bb
NELSON ALEJANDRO PALACIOS GALEANO <nelsonpalacios98055@correo.itm.edu.co>
2020-03-24 22:15:36 -0500

    Add accountController, Usertype, ReportEntity, UserEntity, IUserHeper with the implementation and create views to login

---
+++ Pandemia.Web/Helpers/UserHelper.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Pandemic.Web.Models;
+using Pandemic.Web.Data.Entities;
+using System.Threading.Tasks;
+
+namespace Pandemic.Web.Helpers
+{
+    public class UserHelper : IUserHelper
+    {
+        private readonly SignInManager<UserEntity> _signInManager;
+        private readonly UserManager<UserEntity> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+
+        public UserHelper(
+
+            SignInManager<UserEntity> signInManager,
+            UserManager<UserEntity> userManager,
+            RoleManager<IdentityRole> roleManager
+
+
+            )
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+
+
+        public async Task<SignInResult> LoginAsync(LoginViewModel model)
+        {
+              return await _signInManager.PasswordSignInAsync(
+              model.Username,
+              model.Password,
+              model.RememberMe,
+              false);
+        }
+
+        public async Task LogoutAsync()
+        {
+            await _signInManager.SignOutAsync();
+        }
+        public async Task AddUserToRoleAsync(UserEntity user, string roleName)
+        {
+            await _userManager.AddToRoleAsync(user, roleName);
+        }
+
+        public async Task<IdentityResult> AddUserAsync(UserEntity user, string password)
+        {
+            return await _userManager.CreateAsync(user, password);
+        }
+
+
+        public async Task CheckRoleAsync(string roleName)
+        {
+            bool roleExists = await _roleManager.RoleExistsAsync(roleName);
+            if (!roleExists)
+            {
+                await _roleManager.CreateAsync(new IdentityRole
+                {
+                    Name = roleName
+                });
+            }
+        }
+
+        public async Task<UserEntity> GetUserByEmailAsync(string email)
+        {
+            return await _userManager.FindByEmailAsync(email);
+        }
+
+        public async Task<bool> IsUserInRoleAsync(UserEntity user, string roleName)
+        {
+            return await _userManager.IsInRoleAsync(user, roleName);
+        }
+
+    }
+}
