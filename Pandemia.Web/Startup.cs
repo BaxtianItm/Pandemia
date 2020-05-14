32435415f7c59ce84c5abbaf5f184db3795d5dd8
baxtian7 <sebastiangomezjimenez8@gmail.com>
2020-05-14 17:57:28 -0500

    Add library

--- Pandemia.Web/Startup.cs
+++ Pandemia.Web/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -11,6 +12,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Pandemic.Web.Data;
 using Pandemic.Web.Data.Entities;
 using Pandemic.Web.Helpers;
