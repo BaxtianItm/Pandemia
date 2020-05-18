a4ee6ef5d26def9f150524bd6184c3f82cf18af4
NELSON ALEJANDRO PALACIOS GALEANO <nelsonpalacios98055@correo.itm.edu.co>
2020-05-18 03:17:46 -0500

    Add ValidateData in HomePageViewModel

--- Pandemic.Prism/Pandemic.Prism/ViewModels/HomePageViewModel.cs
+++ Pandemic.Prism/Pandemic.Prism/ViewModels/HomePageViewModel.cs
@@ -1,7 +1,9 @@
-﻿using Pandemic.Common.Services;
+﻿using Pandemic.Common.Models;
+using Pandemic.Common.Services;
 using Pandemic.Prism.Helpers;
 using Prism.Commands;
 using Prism.Navigation;
+using System.Threading.Tasks;
 
 namespace Pandemic.Prism.ViewModels
 {
@@ -11,6 +13,7 @@ namespace Pandemic.Prism.ViewModels
         private readonly IApiService _apiService;
         private bool _isRunning;
         private bool _isEnabled;
+        //private ReportResponse _report;
         private DelegateCommand _addReportCommand;
 
         public HomePageViewModel(
@@ -23,7 +26,13 @@ namespace Pandemic.Prism.ViewModels
             IsEnabled = true;
         }
 
-                public DelegateCommand AddReportCommand => _addReportCommand ?? (_addReportCommand = new DelegateCommand(AddReport));
+        public DelegateCommand AddReportCommand => _addReportCommand ?? (_addReportCommand = new DelegateCommand(AddReport));
+
+        public bool IsRunning
+        {
+            get => _isRunning;
+            set => SetProperty(ref _isRunning, value);
+        }
 
         public bool IsEnabled
         {
@@ -31,11 +40,64 @@ namespace Pandemic.Prism.ViewModels
             set => SetProperty(ref _isEnabled, value);
         }
 
+        public string Document { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        /*public ReportResponse Report
+        {
+            get => _report;
+            set => SetProperty(ref _report, value);
+        }*/
+
         private async void AddReport()
         {
+            bool isValid = await ValidateDataAsync();
+            if (!isValid)
+            {
+                return;
+            }
 
+            IsRunning = true;
+            IsEnabled = false;
 
-            await _navigationService.NavigateAsync("HomePage");
+        }
+
+        private async Task<bool> ValidateDataAsync()
+        {
+            if (string.IsNullOrEmpty(/*Report.*/Document))
+            {
+                await App.Current.MainPage.DisplayAlert(Languages.Error, Languages.DocumentError, Languages.Accept);
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(/*Report.*/FirstName))
+            {
+                await App.Current.MainPage.DisplayAlert(Languages.Error, Languages.FirstNameError, Languages.Accept);
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(/*Report.*/LastName))
+            {
+                await App.Current.MainPage.DisplayAlert(Languages.Error, Languages.LastNameError, Languages.Accept);
+                return false;
+            }
+
+            /* if (double.IsNaN(Report.TargetLatitude))
+             {
+                 await App.Current.MainPage.DisplayAlert(Languages.Error, Languages.AddressError, Languages.Accept);
+                 return false;
+             }*/
+
+            /* if (double.IsNaN(Report.TargetLongitude))
+             {
+                 await App.Current.MainPage.DisplayAlert(Languages.Error, Languages.AddressError, Languages.Accept);
+                 return false;
+             }*/
+
+            return true;
         }
     }
 }
