b63fa19cfa508678481aadf0597532c56dd2f34f
baxtian7 <sebastiangomezjimenez8@gmail.com>
2020-05-24 12:13:59 -0500

    viewModel historial

--- Pandemia.Common/Models/MyReportsRequest.cs
+++ Pandemia.Common/Models/MyReportsRequest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
-
-namespace Pandemic.Common.Models
+﻿namespace Pandemic.Common.Models
 {
     public class MyReportsRequest
     {
--- Pandemic.Prism/Pandemic.Prism/ViewModels/ReportsHistoryPageViewModel.cs
+++ Pandemic.Prism/Pandemic.Prism/ViewModels/ReportsHistoryPageViewModel.cs
@@ -1,11 +1,11 @@
-﻿using Pandemic.Common.Services;
+﻿using Newtonsoft.Json;
+using Pandemic.Common.Helpers;
+using Pandemic.Common.Models;
+using Pandemic.Common.Services;
 using Pandemic.Prism.Helpers;
 using Prism.Commands;
-using Prism.Mvvm;
 using Prism.Navigation;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Pandemic.Prism.ViewModels
 {
@@ -15,6 +15,7 @@ namespace Pandemic.Prism.ViewModels
         private readonly IApiService _apiService;
         private bool _isRunning;
         private bool _isEnabled;
+        private List<ReportResponse> _details;
         private DelegateCommand _addReportCommand;
 
 
@@ -26,15 +27,60 @@ namespace Pandemic.Prism.ViewModels
             _apiService = apiService;
             Title = Languages.CheckHistory;
             IsEnabled = true;
-
+            LoadReports();
         }
 
         public DelegateCommand CreateReportCommand => _addReportCommand ?? (_addReportCommand = new DelegateCommand(CreateReport));
+
+        public bool IsRunning
+        {
+            get => _isRunning;
+            set => SetProperty(ref _isRunning, value);
+        }
 
         public bool IsEnabled
         {
             get => _isEnabled;
             set => SetProperty(ref _isEnabled, value);
+        }
+
+        private async void LoadReports()
+        {
+            IsRunning = true;
+            IsEnabled = false;
+
+            string url = App.Current.Resources["UrlAPI"].ToString();
+            bool connection = await _apiService.CheckConnectionAsync(url);
+            if (!connection)
+            {
+                IsRunning = false;
+                IsEnabled = true;
+                await App.Current.MainPage.DisplayAlert(Languages.Error, Languages.ConnectionError, Languages.Accept);
+                return;
+            }
+
+            UserResponse user = JsonConvert.DeserializeObject<UserResponse>(Settings.User);
+            TokenResponse token = JsonConvert.DeserializeObject<TokenResponse>(Settings.Token);
+
+            MyReportsRequest request = new MyReportsRequest
+            {
+                UserId = user.Id
+            };
+
+            Response response = await _apiService.ListReportsAsync(url, "/api", "/Travel/GetTravel", request, "bearer", token.Token);
+
+            IsRunning = false;
+            IsEnabled = true;
+            if (!response.IsSuccess)
+            {
+                await App.Current.MainPage.DisplayAlert(Languages.Error, response.Message, Languages.Accept);
+                return;
+            }
+
+
+
+
+
         }
 
         private async void CreateReport()
